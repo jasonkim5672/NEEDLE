0546a3dd153c7b4fdb3d0b6c7a6a34836fc26f5a
Jason Kim <jasonkim5672@users.noreply.github.com>
2017-12-02 16:50:10 +0900

    네이버 지도 API 설정 및 Second view에 로드

--- NEEDLE/Controller/SecondViewController.swift
+++ NEEDLE/Controller/SecondViewController.swift
@@ -8,11 +8,58 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
 
+class SecondViewController: UIViewController,NMapViewDelegate, NMapPOIdataOverlayDelegate {
+    
+    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForOverlayItem poiItem: NMapPOIitem!, selected: Bool) -> UIImage! {
+        return NMapViewResources.imageWithType(poiItem.poiFlagType, selected: selected)
+        
+    }
+    
+    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, anchorPointWithType poiFlagType: NMapPOIflagType) -> CGPoint {
+        return NMapViewResources.anchorPoint(withType: poiFlagType)
+    }
+    
+    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForCalloutOverlayItem poiItem: NMapPOIitem!, constraintSize: CGSize, selected: Bool, imageForCalloutRightAccessory: UIImage!, calloutPosition: UnsafeMutablePointer<CGPoint>!, calloutHit calloutHitRect: UnsafeMutablePointer<CGRect>!) -> UIImage! {
+        return nil
+
+    }
+    
+    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, calloutOffsetWithType poiFlagType: NMapPOIflagType) -> CGPoint {
+        return CGPoint(x: 0, y: 0)
+    }
+    
+    var mapView: NMapView?
     override func viewDidLoad() {
         super.viewDidLoad()
+        var colors = [UIColor]()
+        colors.append(UIColor(red: 136, green: 128, blue: 216))
+        colors.append(UIColor(red: 91, green: 143, blue: 191))
+        navigationController?.navigationBar.setGradientBackground(colors: colors)
         // Do any additional setup after loading the view, typically from a nib.
+        mapView = NMapView(frame: self.view.frame)
+        self.navigationController?.navigationBar.isTranslucent = false
+        
+        if let mapView = mapView {
+            // set the delegate for map view
+            mapView.delegate = self
+            
+            // set the application api key for Open MapViewer Library
+            mapView.setClientId("eBknY0lClg3MZUJW1TOB")
+                mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                view.addSubview(mapView)
+        }
+    }
+    public func onMapView(_ mapView: NMapView!, initHandler error: NMapError!) {
+        if (error == nil) { // success
+            // set map center and level
+            mapView.setMapCenter(NGeoPoint(longitude:126.978371, latitude:37.5666091), atLevel:11)
+            
+            // set for retina display
+            mapView.setMapEnlarged(true, mapHD: true)
+        } else { // fail
+            print("onMapView:initHandler: \(error.description)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ NEEDLE/NMHeader.h
@@ -0,0 +1,15 @@
+//
+//  NMHeader.h
+//  NEEDLE
+//
+//  Created by Jason Kim on 2017. 12. 2..
+//  Copyright © 2017년 JasonKim. All rights reserved.
+//
+
+#ifndef NMHeader_h
+#define NMHeader_h
+#import <NMapViewerSDK/NMapView.h>
+#import <NMapViewerSDK/NMapLocationManager.h>
+
+#endif /* NMHeader_h */
+
